fdab511cf2e90e65bfc7ae84407aead9b85074ab
Prasenjit Mohanta <106687836+prasenjitmohanta@users.noreply.github.com>
2025-08-05 15:11:26 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,47 +1,20 @@
-import java.util.*;
-
-
-//Employee class implementing comparable interface
-class Employee implements Comparable<Employee>{
-    String firstName; 
-    String lastName;
-    //constructor
-    public Employee(String firstName,String lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
-    }
-    //Implements the compareTO method to sort by last name 
-    @Override
-    public int compareTo(Employee i){
-        return this.lastName.compareTo(i.lastName);
-    }   
-    //override tosString method to  display enployee names
-    @Override
-    public String toString(){
-        return firstName + " " +lastName;
-    }
-
-   
-}
-
-
-public class sortExample{
+public class nestedTry{
     public static void main(String args[]){
-        //setup array with employee class
-        Employee a[]={
-            new Employee("Kirk","Douglas"),
-            new Employee("Keil","Bruks"),
-            new Employee("houuil","Douglas"),
-            new Employee("Kutts","itse"),
-            new Employee("hhuri","Douglas"),
-        };
+        try{//outer try block
+            int  a = Integer.parseInt(args[0]);
+            int  b = Integer.parseInt(args[1]);
+            int ans =0;
+            try{
+                ans=a/b;
+                System.out.println("the result is "+ans);
 
-        // sort the array with comparable interface
-        Arrays.sort(a);
-
-        //print sorted Employee
-        for(Employee employee:a){
-            System.out.println(employee);
+            }
+            catch(ArithmeticException e){
+                System.out.println("Divided by zero!!");
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Incorrect type of data!!");
         }
     }
 }
